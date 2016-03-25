d9406bbcdb0840109c5849a1eac3285ccbd97285
Kristofer Raske <kristofer.raske@wustl.edu>
2016-03-25 14:41:16 -0500

    Testing javascript integration, It works!
    Now all my code is gone though. Also emscripten causes the browser to
    hang. So that is the next step of the process

--- brogue/src/platform/web-platform.c
+++ brogue/src/platform/web-platform.c
@@ -2,30 +2,12 @@
 
 #include <stdio.h>
 #include <string.h>
-#include <poll.h>
 #include <unistd.h>
 #include <emscripten.h>
 
 #include "platform.h"
 
-#define NUM_POLL_FIELDS         1
-#define RETURN_POLL_NOW         0
-
-#define OUTPUT_SIZE             10
-#define MAX_INPUT_SIZE          4
-#define MOUSE_INPUT_SIZE        4
-#define KEY_INPUT_SIZE          4
-
-enum StatusTypes {
-    DEEPEST_LEVEL_STATUS,
-    GOLD_STATUS,
-    SEED_STATUS,
-    EASY_MODE_STATUS,
-    STATUS_TYPES_NUMBER
-};
-
 extern playerCharacter rogue;
-static struct pollfd fds[NUM_POLL_FIELDS];
 
 static void gameLoop()
 {
@@ -37,73 +19,25 @@ static void web_plotChar(uchar inputChar,
 			  short foreRed, short foreGreen, short foreBlue,
 			  short backRed, short backGreen, short backBlue) {
 
-    // just pack up the output and ship it off to the webserver
-    char outputBuffer[OUTPUT_SIZE];
-
-    char firstCharByte = inputChar >> 8 & 0xff;
-    char secondCharByte = inputChar;
-
-    outputBuffer[0] = (char) xLoc;
-    outputBuffer[1] = (char) yLoc;
-    outputBuffer[2] = firstCharByte;
-    outputBuffer[3] = secondCharByte;
-    outputBuffer[4] = (char) foreRed * 255 / 100;
-    outputBuffer[5] = (char) foreGreen * 255 / 100;
-    outputBuffer[6] = (char) foreBlue * 255 / 100;
-    outputBuffer[7] = (char) backRed * 255 / 100;
-    outputBuffer[8] = (char) backGreen * 255 / 100;
-    outputBuffer[9] = (char) backBlue * 255 / 100;
-
-    fwrite(outputBuffer, sizeof(char), OUTPUT_SIZE, stdout);
+EM_ASM_ARGS({
+  console.log('I got (x,y): ' + [$0, $1]);
+}, xLoc, yLoc);
 
 }
 
 static void sendStatusUpdate(){
-
-    char statusOutputBuffer[STATUS_TYPES_NUMBER * OUTPUT_SIZE];
-
-    unsigned long statusValues[STATUS_TYPES_NUMBER];
-    statusValues[DEEPEST_LEVEL_STATUS] = rogue.deepestLevel;
-    statusValues[GOLD_STATUS] = rogue.gold;
-    statusValues[SEED_STATUS] = rogue.seed;
-    statusValues[EASY_MODE_STATUS] = rogue.easyMode;
-
-    int i;
-    int j;
-    for (i = 0; i < STATUS_TYPES_NUMBER; i++){
-
-        // Coordinates of (255, 255) will let the server and client know that this is a status update rather than a cell update
-        statusOutputBuffer[0] = 255;
-        statusOutputBuffer[1] = 255;
-
-        // The status type
-        statusOutputBuffer[2] = i;
-
-        // I am just going to explicitly send the status big-endian so we can be consistent on the client and server
-        statusOutputBuffer[3] = statusValues[i] >> 24 & 0xff;
-        statusOutputBuffer[4] = statusValues[i] >> 16 & 0xff;
-        statusOutputBuffer[5] = statusValues[i] >> 8 & 0xff;
-        statusOutputBuffer[6] = statusValues[i];
-
-        // The rest is filler so we keep consistent output size
-        for (j = 7; j < OUTPUT_SIZE; j++){
-            statusOutputBuffer[j] = 0;
-        }
-
-        fwrite(statusOutputBuffer, sizeof(char), OUTPUT_SIZE, stdout);
-    }
+  EM_ASM({
+    console.log('status update!');
+  });
 }
 
 // This function is used both for checking input and pausing
     static boolean web_pauseForMilliseconds(short milliseconds)
 {
     emscripten_sleep(milliseconds);
-
-    fds[0].fd = STDIN_FILENO;
-    fds[0].events = POLLIN | POLLPRI;
-    fds[0].revents = 0;
-
-    return (poll(fds, NUM_POLL_FIELDS, RETURN_POLL_NOW) > 0);
+    EM_ASM({
+      console.log('checking for input...');
+    });
 }
 
 static void web_nextKeyOrMouseEvent(rogueEvent *returnEvent, boolean textInput, boolean colorsDance)
@@ -117,32 +51,9 @@ static void web_nextKeyOrMouseEvent(rogueEvent *returnEvent, boolean textInput, 
     // Send a status update of game variables we want on the client
     sendStatusUpdate();
 
-    // ensure entire stream is written out before getting input
-    fflush(stdout);
-
-    char controlBuffer[1];
-    char inputBuffer[MAX_INPUT_SIZE];
-
-    fread(controlBuffer, sizeof(char), 1, stdin);
-    returnEvent->eventType = controlBuffer[0];
-
-    if (returnEvent->eventType == KEYSTROKE){
-        fread(inputBuffer, sizeof(char), KEY_INPUT_SIZE, stdin);
-
-        unsigned short keyCharacter = inputBuffer[0] << 8 | inputBuffer[1];
-
-        returnEvent->param1 = keyCharacter;  //key character
-        returnEvent->controlKey = inputBuffer[2];
-        returnEvent->shiftKey = inputBuffer[3];
-    }
-    else // it is a mouseEvent
-    {
-        fread(inputBuffer, sizeof(char), MOUSE_INPUT_SIZE, stdin);
-        returnEvent->param1 = inputBuffer[0];  //x coord
-        returnEvent->param2 = inputBuffer[1];  //y coord
-        returnEvent->controlKey = inputBuffer[2];
-        returnEvent->shiftKey = inputBuffer[3];
-    }
+    EM_ASM({
+      console.log('injecting input into brogue variables...');
+    });
 
 }
 
