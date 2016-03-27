58e84da91c20a940531cce35603e55dae37c1a3e
Kristofer Raske <kristofer.raske@wustl.edu>
2016-03-27 18:49:46 -0500

    Adding switch in C code to stop waiting

--- brogue/src/platform/web-platform.c
+++ brogue/src/platform/web-platform.c
@@ -19,64 +19,55 @@ static void web_plotChar(uchar inputChar,
   short foreRed, short foreGreen, short foreBlue,
   short backRed, short backGreen, short backBlue) {
 
-    // Reprocess colors to be on scale of 0-255 instead of 0-100
-    foreRed = foreRed * 255 / 100;
-    foreGreen = foreGreen * 255 / 100;
-    foreBlue = foreBlue * 255 / 100;
-    backRed = backRed * 255 / 100;
-    backGreen = backGreen * 255 / 100;
-    backBlue = backBlue * 255 / 100;
+  // Reprocess colors to be on scale of 0-255 instead of 0-100
+  foreRed = foreRed * 255 / 100;
+  foreGreen = foreGreen * 255 / 100;
+  foreBlue = foreBlue * 255 / 100;
+  backRed = backRed * 255 / 100;
+  backGreen = backGreen * 255 / 100;
+  backBlue = backBlue * 255 / 100;
 
-    // EM_ASM_ARGS only allows a max of 8 arguments. So we need to split these up
-    EM_ASM_ARGS({
-      brogue.bridge.plotChar.prepareLoc($0, $1);
-    }, xLoc, yLoc);
+  // EM_ASM_ARGS only allows a max of 8 arguments. So we need to split these up
+  EM_ASM_ARGS({
+    brogue.bridge.plotChar.prepareLoc($0, $1);
+  }, xLoc, yLoc);
 
-    EM_ASM_ARGS({
-      brogue.bridge.plotChar.prepareForeGround($0, $1, $2, $3);
-    }, inputChar, foreRed, foreGreen, foreBlue);
+  EM_ASM_ARGS({
+    brogue.bridge.plotChar.prepareForeGround($0, $1, $2, $3);
+  }, inputChar, foreRed, foreGreen, foreBlue);
 
-    EM_ASM_ARGS({
-      brogue.bridge.plotChar.prepareBackGround($0, $1, $2);
-    }, backRed, backGreen, backBlue);
+  EM_ASM_ARGS({
+    brogue.bridge.plotChar.prepareBackGround($0, $1, $2);
+  }, backRed, backGreen, backBlue);
 
-    EM_ASM({
-      brogue.bridge.plotChar.commitDraw();
-    });
-
-  }
-
-static void sendStatusUpdate(){
   EM_ASM({
-    console.log('status update!');
+    brogue.bridge.plotChar.commitDraw();
   });
+
 }
 
 // This function is used both for checking input and pausing
-    static boolean web_pauseForMilliseconds(short milliseconds)
+static boolean web_pauseForMilliseconds(short milliseconds)
 {
-    emscripten_sleep_with_yield(milliseconds);
-    EM_ASM({
-      console.log('checking for input...');
-    });
+  emscripten_sleep_with_yield(milliseconds);
+  int input_ready = EM_ASM_INT_V({
+    return brogue.state.nextKeyOrMouseReady;
+  });
 
-    return false;
+  return input_ready;
 }
 
 static void web_nextKeyOrMouseEvent(rogueEvent *returnEvent, boolean textInput, boolean colorsDance)
 {
-    // because we will halt execution until we get more input, we definitely cannot have any dancing colors from the server side.
-    colorsDance = false;
+  // because we will halt execution until we get more input, we definitely cannot have any dancing colors from the server side.
+  colorsDance = false;
 
-    // We must avoid the main menu, so we spawn this process with noMenu, and quit instead of going to the menu
-    if (noMenu && rogue.nextGame == NG_NOTHING) rogue.nextGame = NG_QUIT;
+  // We must avoid the main menu, so we spawn this process with noMenu, and quit instead of going to the menu
+  if (noMenu && rogue.nextGame == NG_NOTHING) rogue.nextGame = NG_QUIT;
 
-    // Send a status update of game variables we want on the client
-    sendStatusUpdate();
-
-    EM_ASM({
-      console.log('injecting input into brogue variables...');
-    });
+  EM_ASM({
+    console.log('injecting input into brogue variables...');
+  });
 
 }
 
